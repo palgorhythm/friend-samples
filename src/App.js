91513c2e6d5ccaf2fea12e09e6c3df1afe7a4be2
palgorhythm <jacob@mothership.com>
2020-07-09 20:19:36 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const samplePackData = [
+]
+for(let i = 0; i < 20; i ++){
+  samplePackData.push({title: `Cool/Dope Sample Pack ${i + 1}`, url: 'https://heeeeeeeey.com/', imgUrl: 'https://picsum.photos/500'})
+}
+
+const App = () => (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        friend samples :D
       </header>
+      <div className="sample-packs">
+        { samplePackData.map((samplePack, index) => SamplePack({...samplePack, key: `key-${index}`}))}
+      </div>
     </div>
   );
-}
+const SamplePack = ({title, url, imgUrl, key}) => (
+  <div className="sample-pack" key={key}> 
+    <div className="sample-pack-title">
+      <a href={url}>{title}</a>
+    </div>
+    <img src={imgUrl} className={`sample-pack-${title}`} alt="sample-pack-logo" />
+  </div>
+)
+
 
 export default App;
